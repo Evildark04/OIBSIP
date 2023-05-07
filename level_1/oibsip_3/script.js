1bc001c77f403cec0ae9ed454a32284cba60fc16
Evildark <preethamkrishna464@gmail.com>
2023-05-08 00:40:14 +0530

    final

--- level_1/oibsip_3/script.js
+++ level_1/oibsip_3/script.js
@@ -1,44 +1,57 @@
 function convert() {
-    const fromUnit = document.getElementById("from").value;
-    const toUnit = document.getElementById("to").value;
-    let input = document.getElementById("input").value;
-    let output = document.getElementById("output");
-  
-    if (input === "") {
-      input = 0;
-    }
-  
-    if (fromUnit === "celsius" && toUnit === "fahrenheit") {
-      output.value = input * 1.8 + 32;
-    } else if (fromUnit === "celsius" && toUnit === "kelvin") {
-      output.value = parseInt(input) + 273.15;
-    } else if (fromUnit === "fahrenheit" && toUnit === "celsius") {
-      output.value = ((input - 32) * 5) / 9;
-    } else if (fromUnit === "fahrenheit" && toUnit === "kelvin") {
-      output.value = ((input - 32) * 5) / 9 + 273.15;
-    } else if (fromUnit === "kelvin" && toUnit === "celsius") {
-      output.value = input - 273.15;
-    } else if (fromUnit === "kelvin" && toUnit === "fahrenheit") {
-      output.value = (input - 273.15) * 1.8 + 32;
-    } else {
-      output.value = input;
-    }
+  const fromUnit = document.getElementById("from").value;
+  const toUnit = document.getElementById("to").value;
+  let input = document.getElementById("input").value;
+  let output = document.getElementById("output");
+  let fromUnitText = document.getElementById("from-unit");
+  let toUnitText = document.getElementById("to-unit");
+
+  if (input === "") {
+    input = 0;
   }
-  
-  document.getElementById("from").value = "celsius";
-  document.getElementById("to").value = "fahrenheit";
-  document.getElementById("input").value = 0;
-  convert();
-  
-  document.getElementById("input").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      convert();
-    }
-  });
-  
-  document.addEventListener("keydown", function (event) {
-    if (event.key === "Enter" && document.activeElement === document.body) {
-      convert();
-    }
-  });
-  
+
+  if (fromUnit === "celsius" && toUnit === "fahrenheit") {
+    output.value = input * 1.8 + 32;
+    fromUnitText.textContent = "°C";
+    toUnitText.textContent = "°F";
+  } else if (fromUnit === "celsius" && toUnit === "kelvin") {
+    output.value = parseInt(input) + 273.15;
+    fromUnitText.textContent = "°C";
+    toUnitText.textContent = "°K";
+  } else if (fromUnit === "fahrenheit" && toUnit === "celsius") {
+    output.value = ((input - 32) * 5) / 9;
+    fromUnitText.textContent = "°F";
+    toUnitText.textContent = "°C";
+  } else if (fromUnit === "fahrenheit" && toUnit === "kelvin") {
+    output.value = ((input - 32) * 5) / 9 + 273.15;
+    fromUnitText.textContent = "°F";
+    toUnitText.textContent = "°K";
+  } else if (fromUnit === "kelvin" && toUnit === "celsius") {
+    output.value = input - 273.15;
+    fromUnitText.textContent = "°K";
+    toUnitText.textContent = "°C";
+  } else if (fromUnit === "kelvin" && toUnit === "fahrenheit") {
+    output.value = (input - 273.15) * 1.8 + 32;
+    fromUnitText.textContent = "°K";
+    toUnitText.textContent = "°F";
+  } else {
+    output.value = input;
+  }
+}
+
+document.getElementById("from").value = "celsius";
+document.getElementById("to").value = "fahrenheit";
+document.getElementById("input").value = 0;
+convert();
+
+document.getElementById("input").addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    convert();
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" && document.activeElement === document.body) {
+    convert();
+  }
+});
